bf5ea40f532c4878fe52b52ad1aaa717d54f7add
Eli Yip <yezi.2022@outlook.com>
2023-08-30 21:53:37 +0800

    use wakatime

---
+++ lua/user/configs/wakatime/wakatime.lua
@@ -0,0 +1,3 @@
+return function()
+	require("wakatime").setup()
+end
---
+++ lua/user/plugins/wakatime.lua
@@ -0,0 +1,8 @@
+local wakatime = {}
+
+wakatime["wakatime/vim-wakatime"] = {
+	lazy = true,
+	config = require("user.configs.wakatime.wakatime"),
+}
+
+return wakatime
